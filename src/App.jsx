c1dca6f952ed8ef0c7ece32eace39bef203dac56
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2026-02-02 16:54:11 +0100

    70% Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,79 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Wake up to Eat", completed: false }
+  ])
+
+  const [input, setInput] = useState("")
+
+  const addTask = () => {
+    if (input.trim() === "") return;
+
+    setTasks([
+      ...tasks,
+      { id: Date.now(), text: input, completed: false }
+    ])
+    setInput("")
+  }
+
+  const toggleTaskCompletion = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ))
+  }
+
+  const [filter, setFilter] = useState("all")
+
+  const visibleTasks = tasks.filter(task => {
+    if (filter === 'all') return tasks;
+    if (filter === 'active') return !task.completed;
+    if (filter === 'completed') return task.completed;
+  });
+
 
   return (
     <>
       <main className='size bg-primary border-primary text-primary content'>
         <div className='header'>
-          <h1><img src="/public/icons8-tasklist-24.png"/>To-Do List</h1>
+          <h1><img className='mr-2' src="/public/icons8-to-do-list-38.png" />To-Do List</h1>
           <p>Manage your tasks with state and event handling</p>
         </div>
 
         <div className='form'>
-          <input type="text" placeholder='Add a new task.....' />
-          <button><img src="/public/icons8-plus-36.png" alt="Add Task" /></button>
+          <input type="text" placeholder='Add a new task.....'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTask();
+              }
+            }} />
+          <button onClick={addTask}><img src="/public/icons8-add-36.png" alt="Add Task" /></button>
         </div>
 
         <div className='mid-break'>
-          <p>Active Tasks</p>
-          <p>Completed</p>
+          <p>({tasks.filter(task => !task.completed).length}) Active Tasks</p>
+          <p>({tasks.filter(task => task.completed).length}) Completed</p>
         </div>
 
         <div className='btns-3'>
-          <button className='btn'>All (x)</button>
-          <button className='btn'>Active (x)</button>
-          <button className='btn'>Completed (x)</button>
+          <button className={'btn' + (filter === 'all' ? ' bg-white/90' : '')} onClick={() => setFilter('all')}><img src='/public/icons8-to-do-list-24.png'/> All ({tasks.length})</button>
+          <button className={'btn' + (filter === 'active' ? ' bg-white/90' : '')} onClick={() => setFilter('active')}><img src='/public/icons8-active-24.png' /> Active ({tasks.filter(task => !task.completed).length})</button>
+          <button className={'btn' + (filter === 'completed' ? ' bg-white/90' : '')} onClick={() => setFilter('completed')}><img src='/public/icons8-done-24.png'/> Completed ({tasks.filter(task => task.completed).length})</button>
         </div>
 
         <div className='task'>
-          <div className='task-bar'>
-            <input type="checkbox" id="task1" name="task1" value="Task 1"/>
-            <label for="task1"> Wake Up to Eat</label>
-          </div>
-          <div className='task-bar'>
-            <input type="checkbox" id="task2" name="task2" value="Task 2"/>
-            <label for="task2"> Go to the Gymnasium</label>
-          </div>
-          <div className='task-bar'>
-            <input type="checkbox" id="task3" name="task3" value="Task 3"/>
-            <label for="task3"> Get Ready for Work</label>
-          </div>
+          {visibleTasks.map((task) => (
+            <div className='task-bar' key={task.id}>
+              <input type="checkbox" checked={task.completed}
+                onChange={() => toggleTaskCompletion(task.id)}
+              />
+              <label className={task.completed ? 'line-through text-gray-500' : ''}>{task.text}</label>
+            </div>
+          ))}
         </div>
 
       </main>
