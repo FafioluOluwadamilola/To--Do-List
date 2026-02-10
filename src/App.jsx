7d2d05b9cfa7115c18e86943019706dffe8849b5
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2026-02-10 12:27:36 +0100

    Done with the edit & delete, also media query

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,33 @@ function App() {
 
   const [filter, setFilter] = useState("all")
 
+
   const visibleTasks = tasks.filter(task => {
     if (filter === 'all') return tasks;
     if (filter === 'active') return !task.completed;
     if (filter === 'completed') return task.completed;
   });
+
+  const [editingId, setEditingId] = useState(null)
+  const [editValue, setEditValue] = useState("")
+
+  const startEdit = (task) => {
+    setEditingId(task.id)
+    setEditValue(task.text)
+  }
+
+  const saveEdit = () => {
+    setTasks(tasks.map(task =>
+      task.id === editingId ? { ...task, text: editValue } : task
+    ))
+    setEditingId(null)
+    setEditValue("")
+  }
+
+  const cancelEdit = () => {
+    setEditingId(null)
+    setEditValue("")
+  }
 
 
   return (
@@ -60,9 +82,9 @@ function App() {
         </div>
 
         <div className='btns-3'>
-          <button className={'btn' + (filter === 'all' ? ' bg-white/90' : '')} onClick={() => setFilter('all')}><img src='/public/icons8-to-do-list-24.png'/> All ({tasks.length})</button>
+          <button className={'btn' + (filter === 'all' ? ' bg-white/90' : '')} onClick={() => setFilter('all')}><img src='/public/icons8-to-do-list-24.png' /> All ({tasks.length})</button>
           <button className={'btn' + (filter === 'active' ? ' bg-white/90' : '')} onClick={() => setFilter('active')}><img src='/public/icons8-active-24.png' /> Active ({tasks.filter(task => !task.completed).length})</button>
-          <button className={'btn' + (filter === 'completed' ? ' bg-white/90' : '')} onClick={() => setFilter('completed')}><img src='/public/icons8-done-24.png'/> Completed ({tasks.filter(task => task.completed).length})</button>
+          <button className={'btn' + (filter === 'completed' ? ' bg-white/90' : '')} onClick={() => setFilter('completed')}><img src='/public/icons8-done-24.png' /> Completed ({tasks.filter(task => task.completed).length})</button>
         </div>
 
         <div className='task'>
@@ -71,7 +93,45 @@ function App() {
               <input type="checkbox" checked={task.completed}
                 onChange={() => toggleTaskCompletion(task.id)}
               />
-              <label className={task.completed ? 'line-through text-gray-500' : ''}>{task.text}</label>
+
+
+              {editingId === task.id ? (  
+                  <>
+                    <input
+                      type="text"
+                      value={editValue}
+                      onChange={(e) => setEditValue(e.target.value)}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") saveEdit();
+                        if (e.key === "Escape") cancelEdit();
+                      }}
+                    />
+                    <div className='ml-auto flex flex-row items-center gap-2'>
+                      <img src='/public/icons8-enter-48.png' alt="Save Edit" onClick={saveEdit} />
+                      <p>
+                        <img src='/public/icons8-cancel-48.png' alt="Cancel Edit" onClick={cancelEdit} />
+                      </p>
+                    </div>
+                  </>
+
+                ) : (
+
+             <>
+               <label className={task.completed ? 'line-through text-gray-500' : ''}>{task.text}</label>
+               <div className='ml-auto flex gap-2'>
+                 <img src='/public/icons8-edit-16.png' alt="Edit Task"  onClick={() => startEdit(task)}/>
+                 <img
+                   src='/public/icons8-trash-16 (1).png' alt="Delete Task"
+                   onClick={() => {
+                     const confirmDelete = window.confirm("Are you sure you want to delete this task?");
+                     if (confirmDelete) {
+                       setTasks(tasks.filter(p => p.id !== task.id))
+                     }
+                   }}
+                 />
+               </div>
+             </>
+              )}
             </div>
           ))}
         </div>
